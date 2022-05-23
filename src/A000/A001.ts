faba45276407d775406ed8414cfd4579cb1e7c45
Hugo Sotduto <66806744+hugostoduto@users.noreply.github.com>
2022-05-23 10:22:58 -0300

    Update A001.ts

--- src/A000/A001.ts
+++ src/A000/A001.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 // Tipos básicos (aqui ocorre inferência de tipos)
-let nome: string = 'Luiz'; // Qualquer tipo de strings: '' "" ``
+let nome: string = 'Hugo'; // Qualquer tipo de strings: '' "" ``
 let idade: number = 0b1010; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
 let adulto: boolean = true; // true ou false
 let simbolo: symbol = Symbol('qualquer-symbol'); // symbol
